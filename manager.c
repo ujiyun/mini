4d2bc82e616253eeb94d3bb805608fce5c94d4f7
ujiyun <a01052919770@gmail.com>
2023-04-23 03:06:16 +0900

    load

--- manager.c
+++ manager.c
@@ -58,6 +58,14 @@ int loadData(Product *p){
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
 
+if (fp == NULL) {
+        printf("=> 파일 없음\n");
+        return 0;
+    }
+    for (int i=0; i < 100; i++) {
+        if (feof(fp)) break;
+        fscanf(fp, "%s %d %d", p[i].name, &p[i].weight, &p[i].price);
+    }
 
 
 
